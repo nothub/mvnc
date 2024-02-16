ae287aea0092ebfdac879d10fcb80ccd814f5d5f
Florian Hübner <code@hub.lol>
2024-02-16 19:37:59 +0100

    reposilite testcontainer

---
+++ actions_test.go
@@ -0,0 +1,42 @@
+package mvnc
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestWithReposilite(t *testing.T) {
+
+	req := testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: "dzikoysk/reposilite:3.5.6",
+			Cmd: []string{
+				"--port", "8080",
+				"--token", "test:foobar",
+				"--plugin-directory", "/plugins",
+			},
+			ExposedPorts: []string{"8080/tcp"},
+			WaitingFor:   wait.ForLog("For help, type 'help' or '?'"),
+		},
+		Started: true,
+	}
+
+	ctx := context.Background()
+	tco, err := testcontainers.GenericContainer(ctx, req)
+
+	if err != nil {
+		log.Fatalf("Unable to start reposilite: %s", err)
+	}
+	defer func() {
+		if err := tco.Terminate(ctx); err != nil {
+			log.Fatalf("Unable to stop reposilite: %s", err)
+		}
+	}()
+
+	// do stuff
+
+}
